8bdd613afbbdbd2ec9145cf3173063e52b515619
Emmanuel Nkrumah-Sarpong <nkrumahthis@gmail.com>
2024-02-02 22:58:59 +0000

    basic form

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+  <head>
+    <title>Tiny Urls</title>
+  </head>
+  <body>
+    <div>
+      <div><h1>Tiny Urls</h1></div>
+      <div>
+        <form method="post" action="/">
+          <label for="source_url">Source url</label> 
+          <input name="source_url" />
+
+          <label for="key">Key</label>
+          <div><span>https://tinyurls.com/</span><input placeholder="key" name="key" />
+          <button type="submit">Create</button>
+        </form>
+    </div>
+  </body>
+</html>
+
+<?php
+
+
